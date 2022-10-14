0acb7b5111d63e75b73e3feced5d230f301ee55b
TiieuTiien <phamquoctien1903@gmail.com>
2022-10-14 23:03:24 +0700

    Commited

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    // main => ctrl + khoang trang => enter 
+    public static void main(String[] args) {
+        // sysout => ctrl + khoang trang => enter
+        System.out.println("Hello World!");
+    }    
+}
---
+++ src/KhaiBao.java
@@ -0,0 +1,12 @@
+public class KhaiBao {
+    public static void main(String[] args) {
+        String thongbao = "Hello";
+        String HoVaTen = "Pham Quoc Tien";
+        String tenSach;
+        tenSach = "Java khai bao";
+        
+        System.out.println("Thong bao: " + thongbao);
+        System.out.println("Ho & Ten: " + HoVaTen);
+        System.out.println("Ten sach: " + tenSach);
+    }
+}
