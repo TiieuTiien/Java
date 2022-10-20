2cb281f21d634ff43c01bf1b203e4cb614fcbadc
TiieuTiien <phamquoctien1903@gmail.com>
2022-10-20 20:55:26 +0700

    Commited

--- src/_53_MangJava.java
+++ src/_53_MangJava.java
@@ -5,17 +5,24 @@ public class _53_MangJava {
         //Kieu nguyen thuy  
         int[] mang1 ={1, 2, 3};
 
-        //Copy mang
+        //Copy mang cach 1 - dung =
         int[] mang1_a = mang1;
         mang1_a[0] = 100;
 
         System.out.println("Mang 1 la: " + Arrays.toString(mang1));
         System.out.println("Mang 1 a la: " + Arrays.toString(mang1_a));
 
-        //clone
+        //Copy mang cach 2 - dung clone
         int[] mang1_b = mang1.clone();
         mang1_b[0] = 50;
         
         System.out.println("Mang 1 b la: " + Arrays.toString(mang1_b));
+
+        //Copy mang cach 3 - dung System.arraycopy
+        int[] mang1_c = new int[mang1.length];
+        System.arraycopy(mang1, 0, mang1_c, 0, mang1.length);
+        mang1_c[0] = 90;
+
+        System.out.println("Mang 1 c: " + Arrays.toString(mang1_c));
     }
 }
