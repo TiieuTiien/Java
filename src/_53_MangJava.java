85e9056b72c3a578eabe77f46a27e08f4e530e1f
TiieuTiien <phamquoctien1903@gmail.com>
2022-10-20 20:16:39 +0700

    Commited

--- src/_53_MangJava.java
+++ src/_53_MangJava.java
@@ -7,8 +7,15 @@ public class _53_MangJava {
 
         //Copy mang
         int[] mang1_a = mang1;
+        mang1_a[0] = 100;
 
         System.out.println("Mang 1 la: " + Arrays.toString(mang1));
-        System.out.println("Mang 1a la: " + Arrays.toString(mang1_a));
+        System.out.println("Mang 1 a la: " + Arrays.toString(mang1_a));
+
+        //clone
+        int[] mang1_b = mang1.clone();
+        mang1_b[0] = 50;
+        
+        System.out.println("Mang 1 b la: " + Arrays.toString(mang1_b));
     }
 }
