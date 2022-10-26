e13019c1bf622a9b749b26ae384ce0fd79fa12ec
TiieuTiien <phamquoctien1903@gmail.com>
2022-10-26 23:26:35 +0700

    Commited

--- src/BaoDongBacCau.java
+++ src/BaoDongBacCau.java
@@ -1,68 +1,65 @@
 import java.util.*;
 
 public class BaoDongBacCau{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Hay nhap vao m: ");
+        int m = sc.nextInt();
+        short[][] matrix1 = new short[m][m];
+        short[][] matrix2 = new short[m][m];
+        short[][] matrixMultiply = new short[m][m];
+        short[][] sum = new short[m][m];
 
-    public static void Out(int[][] baoDong, int size){
-        for (int row = 0; row < size; row++) {
-            for (int column = 0; column < size; column++){
-                System.out.print(baoDong[row][column] + " ");
+        In(matrix1, matrix2, sum, m, sc);
+        multiply(matrix1, matrix2, matrixMultiply, sum);
+
+    }
+    public static void In(short[][] a, short[][] b, short[][] d, int m, Scanner sc) {  
+        int i = 0, j = 0;
+        System.out.println("Input elements of the matrix:");
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < m; j++) {
+                a[i][j] = sc.nextShort();
             }
-            System.out.println("\n");
+            b[i] = a[i].clone();
+            d[i] = a[i].clone();
         }
     }
-
-    public static void copyBaoDong(int[][] baoDongBacCau, int[][] baoDongBacCau2){
-        for(int i = 0; i < baoDongBacCau.length; i++)
-            baoDongBacCau2[i] = baoDongBacCau[i].clone();
-    }
-    public static void main(String[] args) {
-        // System.out.print("Hay nhap vao m: ");
-        // int m = sc.nextInt();
-        // int k = sc.nextInt();
-        // int temp = 0;
-        
-        // int[][] baoDongBacCau = new int[m][m];
-
-        // for (int row = 0; row < m; row++) {
-        //     for (int column = 0; column < m; column++){
-        //         baoDongBacCau[row][column] = sc.nextInt();
-        //     }
-        // }
-        // System.out.println("\n");
-
-        // int[][] baoDongBacCau2 = new int[baoDongBacCau.length][];
-        // copyBaoDong(baoDongBacCau, baoDongBacCau2);
-        int[][] a = new int[4][4];
-        int[][] b = new int[4][4];
-        int[][] c = new int[4][4];
-        
-        In(a, 4);
-        In(b, 4);
-        
-            for (int row = 0; row < 3; row++) {
-                for (int column = 0; column < 3; column++){
-                    c[row][column] = 0;
-                    for (int i = 0; i < 3; i++){
-                        c[row][column] += a[row][i]*b[i][column];
+    public static void multiply(short[][] a, short[][] b, short[][] c, short[][] d) {
+        assert a[0].length == c.length: "width of matrix one must be equal to height of matrix two";
+        System.out.println("");
+        //takes the dot product of the rows and columns and adds them to output matrix
+        for (short loop = 0; loop < 3; loop++){
+            System.out.println("M (R" + (loop + 2) + ")");
+            for(short i = 0; i<a.length; i++) {
+                for(short j = 0; j< c[0].length; j++) {
+                    c[i][j] = 0;
+                    for(short k = 0; k < c[0].length; k++) {
+                        c[i][j] += a[i][k] * b[k][j];
                     }
-                    // if (temp >= 1){
-                    //     baoDongBacCau2[row][column] = 1;
-                    // }else{
-                    //     baoDongBacCau2[row][column] = 0;
-                    // }
+                    d[i][j] += c[i][j];
+                    c[i][j] = (short) (c[i][j] > 0 ? 1 : 0);
+                    d[i][j] = (short) (d[i][j] > 0 ? 1 : 0);
                 }
             }
-        
-        Out(c, 4);
-    }
-
-    public static void In(int[][] a, int size){
-        Scanner sc = new Scanner(System.in);
-        for (int row = 0; row < size; row++) {
-            for (int column = 0; column < size; column++){
-                a[row][column] = sc.nextInt();
+            for(short l = 0; l< c.length; l++) {
+                for(short m = 0; m< c[0].length; m++) {
+                    b[l][m] = c[l][m];
+                }
             }
+            Out(c);
         }
-        sc.close();
+        System.out.println("\nM(R*)");
+        Out(d);
+        
+    }
+    public static void Out(short[][] c){
+        for(short l = 0; l< c.length; l++) {
+            for(short m = 0; m< c[0].length; m++) {
+                System.out.print(c[l][m] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
     }
 }
--- src/test.java
+++ src/test.java
@@ -1,8 +1,47 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
-        // khai báo một mảng 2 chiều có 3 dòng và 2 cột
-        int diem[][] = {{1, 2}, {3, 4}, {5, 6}};
-             
-        System.out.println("Phần tử nằm ở dòng 2 và cột 1 trong mảng diem là " + diem[2][1]);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap m: ");
+        int m = sc.nextInt();
+
+        int[][] a = new int[m][m];
+        int[][] b = new int[m][m];
+        int[][] c = new int[m][m];
+        In(a, m);
+        In(b, m);
+
+
+        for (int row = 0; row < m; row++) {
+            for (int column = 0; column < m; column++){
+                c[row][column] = 0;
+                for (int i = 0; i < m; i++){
+                    c[row][column] += a[row][i]*b[i][column];
+                }
+            }
+        }
+        
+        Out(c, m);
+        sc.close();
+    }
+
+    public static void In(int[][] a, int size){
+        Scanner sc = new Scanner(System.in);
+        for (int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++){
+                a[row][column] = sc.nextInt();
+            }
+        }
+        sc.close();
+    }
+    
+    public static void Out(int[][] a, int size){
+        for (int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++){
+                System.out.print(a[row][column] + " ");
+            }
+            System.out.println("\n");
+        }
     }
 }
